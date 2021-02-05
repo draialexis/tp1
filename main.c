9b979abadaf9e8f7d9adfef45b3a0361ecaa73b0
Alexis DRAI <drai.alexis@gmail.com>
2021-02-05 11:44:37 +0100

    Fix 'next char' feature in ex4
    'next char' was showing something other than a letter
    when the char itself was 'z' or 'Z'

--- main.c
+++ main.c
@@ -85,21 +85,17 @@ int main() {
     printf("Donnez un charactere (seul le premier charactere donne sera pris en compte) :");
     scanf(" %c", &c);
 
-    if ((c < 65) || ((c > 90) && (c < 97)) || (c > 122))
-    {
+    if ((c < 65) || ((c > 90) && (c < 97)) || (c > 122)) {
         printf("ce n'est pas une lettre\n");
         return 0;
-    }
-    else
-    {
+    } else {
         printf("code ASCII : %d\n", c);
-        printf("charactere suivant : %c\n", c + 1);
-        if (c <= 90)
-        {
-            printf("charactere en minuscule : %c\n", c + 32);
+        if (c != 90 && c != 122) {
+            printf("charactere suivant : %c\n", c + 1);
         }
-        else if (c >= 97)
-        {
+        if (c <= 90) {
+            printf("charactere en minuscule : %c\n", c + 32);
+        } else if (c >= 97) {
             printf("charactere en majuscule : %c\n", c - 32);
         }
     }
